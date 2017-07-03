8e6bcf00e75ca2e7a420733e14ed6312d21fe261
hackeru <hackerudroids@gmail.com>
2017-07-03 18:21:00 +0300

    Ver 01.06

--- app/src/main/java/com/arichafamily/shoppingfun/fragments/ListProductsFragment.java
+++ app/src/main/java/com/arichafamily/shoppingfun/fragments/ListProductsFragment.java
@@ -2,20 +2,34 @@ package com.arichafamily.shoppingfun.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.EditText;
 
 import com.arichafamily.shoppingfun.R;
 import com.arichafamily.shoppingfun.models.UserList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListProductsFragment extends Fragment {
 
+    @BindView(R.id.rvProducts)
+    RecyclerView rvProducts;
+    @BindView(R.id.fabAddProduct)
+    FloatingActionButton fabAddProduct;
+    @BindView(R.id.etProductName)
+    EditText etProductName;
+    Unbinder unbinder;
     private UserList userList;
 
 
@@ -39,9 +53,21 @@ public class ListProductsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         userList = getArguments().getParcelable("list");
-        Toast.makeText(getContext(), userList.toString(), Toast.LENGTH_SHORT).show();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_products, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_products, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.fabAddProduct)
+    public void onAddProductClicked() {
+        String productName = etProductName.getText().toString();
+
+    }
 }
